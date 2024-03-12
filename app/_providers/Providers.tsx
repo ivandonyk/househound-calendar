6aeb01f765b11c230bae46f0a58200f6b38653f8
Ivan <ivandonyk@gmail.com>
2024-03-13 00:44:51 +0500

    start week from monday fix

--- app/_providers/Providers.tsx
+++ app/_providers/Providers.tsx
@@ -10,10 +10,12 @@ import "@/app/_lib/firebase/firebase"
 import UserProvider from "@/app/_context/UserContext"
 import ModalsProvider from "@/app/_context/ModalContext"
 import CalendarProvider from "@/app/_context/CalendarContext"
+import moment from "moment"
 
 export function Providers({
     children,
 }: { children: React.ReactNode | React.ReactNode[] }) {
+    moment.updateLocale("en-US", { week: { dow: 1 } })
     return (
         <LocalizationProvider dateAdapter={AdapterMoment}>
             <UserProvider>
