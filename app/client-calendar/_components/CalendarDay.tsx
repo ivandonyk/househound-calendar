dbc9dd1e163949897e7ea17f6d660b6aadb310a9
Ivan <ivandonyk@gmail.com>
2024-03-16 00:25:35 +0500

    do not hihglight past dates

--- app/client-calendar/_components/CalendarDay.tsx
+++ app/client-calendar/_components/CalendarDay.tsx
@@ -28,13 +28,15 @@ const CalendarDay: React.FC<ICalendarDayProps> = ({
         if(available) setSelectedDate?.(props.day)
     }
 
+    const isPast = props.day.isBefore(moment().clone().set("hour",0).set("minute", 0).set("second", 0).set("millisecond", 0))
+
     return (
         <div className="h-max w-full">
             <PickersDay 
                 {...props}
                 className={classNames(
                     "!font-[400] !text-[14px] md:!text-[17px] !leading-[22px] md:!leading-[20px]",
-                    { "!bg-white !text-blue-4": available && !(selectedDate?.format("dddd DD yyyy") === props.day.format("dddd DD yyyy")) },
+                    { "!bg-white !text-blue-4": available && !isPast && !(selectedDate?.format("dddd DD yyyy") === props.day.format("dddd DD yyyy")) },
                     { "!text-gray-10 pointer-events-none": !available },
                     { "!font-[600]": moment().format("dddd DD yyyy") === props.day.format("dddd DD yyyy") },
                     { "!bg-blue-4 !text-white": selectedDate?.format("dddd DD yyyy") === props.day.format("dddd DD yyyy") }
